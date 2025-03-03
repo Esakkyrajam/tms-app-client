da4cb721f4bc58215a21372c39c8f91836211d35
Esakkyrajam <esakkyrajam@gmail.com>
2025-03-03 11:25:14 +0530

    first commit dfg

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = class Configuration {
-    GATEWAY_URL = 'http://localhost:3000/api/rest/tms/dataaccess/1.0.0/';
-    GATEWAY_USERNAME = 'QP0192923232LD';
-    GATEWAY_PASSWORD = '927NBGJJ0283HKA74782';
-    HEADERS = {
-        MERCHANT_KEY: 'BASE-MERCHANT-KEY',
-        TENANT_KEY: 'BASE-TENANT-KEY'
-    }
-}
+  GATEWAY_URL = "https://tms-app.onrender.com/api/rest/tms/dataaccess/1.0.0/";
+  GATEWAY_USERNAME = "QP0192923232LD";
+  GATEWAY_PASSWORD = "927NBGJJ0283HKA74782";
+  HEADERS = {
+    MERCHANT_KEY: "BASE-MERCHANT-KEY",
+    TENANT_KEY: "BASE-TENANT-KEY",
+  };
+};
